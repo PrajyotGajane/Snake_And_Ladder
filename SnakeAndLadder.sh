400c0e076c74148774c2b2c8475eef67a93a7142
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-09 15:40:51 +0530

    Updated the code

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -7,21 +7,30 @@ function dice(){
 	return $diceValue
 }
 dice
+diceCap=$?
 # 0 is No Play, 1 is Ladder, 2 is a Snake
 No_play=0
 Ladder=1
 Snake=2
+
 function board(){
 	option=$((RANDOM % 3))
 	if [[ $option -eq $No_play ]]
 	then
-		echo "No play"
+		echo
+		echo "No play: No change in the position"
 	elif [[ $option -eq $Ladder ]]
 	then
+		echo
 		echo "Reached a ladder"
+		player1=$(($player1 + $diceCap))
+		echo "Player position is :"$player1
 	elif [[ $option -eq $Snake ]]
 	then
+		echo
 		echo "Eaten by a snake"
+		player1=$(($player1 - $diceCap))
+		echo "Player position is :"$player1
 	fi
 }
 board
