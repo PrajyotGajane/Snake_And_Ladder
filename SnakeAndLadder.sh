f994fad4bfead1ca63d0aa99d3cdc8d2b143132e
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-09 15:19:13 +0530

    UC 2 completed

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,3 +1,8 @@
 echo "Welcome to snake and ladder game"
 player1=0
 echo "Position of player is $player1"
+function dice(){
+	diceValue=$(($((RANDOM % 6)) + 1))
+	echo "Value of rolled dice :"$diceValue
+}
+dice
