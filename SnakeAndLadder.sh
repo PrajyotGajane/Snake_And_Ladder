9e2bcaee838656c81a1b4c190a9c659e0e612f24
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-09 15:28:15 +0530

    UC 3 completed

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -4,5 +4,24 @@ echo "Position of player is $player1"
 function dice(){
 	diceValue=$(($((RANDOM % 6)) + 1))
 	echo "Value of rolled dice :"$diceValue
+	return $diceValue
 }
 dice
+# 0 is No Play, 1 is Ladder, 2 is a Snake
+No_play=0
+Ladder=1
+Snake=2
+function board(){
+	option=$((RANDOM % 3))
+	if [[ $option -eq $No_play ]]
+	then
+		echo "No play"
+	elif [[ $option -eq $Ladder ]]
+	then
+		echo "Reached a ladder"
+	elif [[ $option -eq $Snake ]]
+	then
+		echo "Eaten by a snake"
+	fi
+}
+board
